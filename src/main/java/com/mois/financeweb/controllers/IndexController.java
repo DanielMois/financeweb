27b76485744327c97c3a3aa930713aa92ce883a2
DanielMois <danielmois1998@gmail.com>
2024-02-13 20:34:14 -0300

    Configuring register and login - v1

--- src/main/java/com/mois/financeweb/controllers/IndexController.java
+++ src/main/java/com/mois/financeweb/controllers/IndexController.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class IndexController {
 
-    @GetMapping("/home")
-    public String home() {
-        return "home";
+    @GetMapping("/")
+    public String showIndexPage() {
+        return "index";
     }
+
+
 }
